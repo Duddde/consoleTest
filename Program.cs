4187650438effbe5c9974a3c119206b5e582d118
Pierre Dufour <pierre.dufour@exakis-nelite.com>
2025-06-12 09:26:18 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Artza!");
-Console.WriteLine("This is a simple console application to demonstrate the use of C#.");
-Console.WriteLine("It is built using .NET 8.0 and C# 12.0.");
